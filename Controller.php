5213d7a4218fb5c53fb6889e940fc689693b42b2
Pouyan <poepno2003@gmail.com>
2014-04-28 13:12:58 +0200

    some changes

--- Controller.php
+++ Controller.php
@@ -6,7 +6,7 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
  *
  */
-namespace Piwik\Plugins\MyPlugin;
+namespace Piwik\Plugins\SubdomainDashboard;
 
 use Piwik\View;
 
@@ -18,7 +18,7 @@ class Controller extends \Piwik\Plugin\Controller
 
     public function index()
     {
-        $view = new View('@MyPlugin/index.twig');
+        $view = new View('@SubdomainDashboard/index.twig');
         $this->setBasicVariablesView($view);
         $view->answerToLife = '42';
 
