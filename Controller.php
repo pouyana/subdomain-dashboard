36c5b79544601e770c7027e37ff40242359acf5a
Pouyan <popeno2003@gmail.com>
2014-04-28 16:32:21 +0200

    contorller under AdminController

--- Controller.php
+++ Controller.php
@@ -8,12 +8,31 @@
  */
 namespace Piwik\Plugins\SubdomainDashboard;
 
+use Exception;
+use Piwik\Common;
+use Piwik\Config;
+use Piwik\Cookie;
+use Piwik\IP;
+use Piwik\Mail;
+use Piwik\Nonce;
+use Piwik\Notification;
+use Piwik\Piwik;
+use Piwik\Plugins\UsersManager\API as APIUsersManager;
+use Piwik\Plugins\UsersManager\UsersManager;
+use Piwik\ProxyHttp;
+use Piwik\QuickForm2;
+use Piwik\Session;
+use Piwik\SettingsPiwik;
+use Piwik\Url;
 use Piwik\View;
 
+require_once PIWIK_INCLUDE_PATH . '/core/Config.php';
+
+
 /**
- *
+ * The Subdomain Dashboard must be shown in the Admin Controller
  */
-class Controller extends \Piwik\Plugin\Controller
+class Controller extends \Piwik\Plugin\ControllerAdmin
 {
 
     public function index()
